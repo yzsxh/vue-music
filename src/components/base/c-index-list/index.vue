962eca989db8603f8ece192b90291db9f70d900d
yzsxh <1223924896@qq.com>
2023-06-12 22:34:32 +0800

    feat: 添加歌手列表页面

---
+++ src/components/base/c-index-list/index.vue
@@ -0,0 +1,69 @@
+<template>
+  <c-scroll class="c-index-list">
+    <ul>
+      <li class="group" v-for="group in data" :key="group.title">
+        <h2 class="title">{{ group.title }}</h2>
+        <ul>
+          <li class="item" v-for="item in group.list" :key="item.id">
+            <img v-lazy="item.pic" alt="" class="avatar" />
+            <span class="name">{{ item.name }}</span>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </c-scroll>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+import CScroll from '@/components/base/c-scroll'
+
+defineProps({
+  data: {
+    type: Array,
+    default: () => []
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.c-index-list {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  background: $color-background;
+  .group {
+    padding-bottom: 30px;
+    background-color: #fff;
+
+    .title {
+      height: 30px;
+      line-height: 30px;
+      padding-left: 20px;
+      font-size: $font-size-small;
+      /* color: $color-text-l; */
+      color: #fff;
+      /* background: $color-background; */
+      background: $color-theme;
+    }
+    .item {
+      display: flex;
+      align-items: center;
+      padding: 20px 0 0 30px;
+      background-color: #fff;
+      border-bottom: 1px solid #f2f2f2;
+      .avatar {
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+      }
+      .name {
+        margin-left: 20px;
+        color: $color-text-l;
+        font-size: $font-size-medium;
+      }
+    }
+  }
+}
+</style>
--- src/views/singer/index.vue
+++ src/views/singer/index.vue
@@ -1,7 +1,27 @@
 <template>
-  <div class="singer">singer</div>
+  <div class="singer" v-loading="!singers.length">
+    <c-index-list :data="singers"></c-index-list>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, onMounted } from 'vue'
+import CIndexList from '@/components/base/c-index-list'
+import { getSingerList } from '@/service/singer'
 
-<style lang="scss" scoped></style>
+const singers = ref([])
+
+onMounted(async () => {
+  const res = await getSingerList()
+  singers.value = res.singers
+})
+</script>
+
+<style lang="scss" scoped>
+.singer {
+  position: fixed;
+  width: 100%;
+  top: 88px;
+  bottom: 0;
+}
+</style>
